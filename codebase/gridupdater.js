5df2b1e78df96dfab62c8f02e82eac5a11c52a0d
csera <csera@ucsd.edu>
2015-06-29 14:19:41 -0700

    Partial fixes: grid expanding & uploading to viewers
    Expanding the grid via the "Control Panel" now mostly works.
    Now able to access functions inside the Webix defined iframes with
    $$('object id').getWindow().method().  However, it is running into the
    same browser compatibility problems as when doing this with HTML
    iframes.
    
    Known Bugs:
    1) With large grids, a row will sometimes be skipped when adding
    columns.
    2) Chrome won't accept uploads to iframes when just running Hydra
    locally.  Origins for the primary frame and iframe are both "null" ->
    same origin violation.
    3) IE won't display the compound (CTX) that viewers are set to display
    by default.  Uploaded files get displayed strangely and most viewer
    controls don't work.
    
    ToDo:
    Fix the above bugs
    Allow grid downsizing
    Allow a file to be uploaded to a selected viewer
    
    Misc:
    If the mouse is being used for compound manipulation and is moved
    outside the viewer before release, the viewer will continue interacting
    with the mouse whenever cursor is in its bounds until the viewer is
    clicked again.  Consider making a global mouse state variable in the
    future?

--- codebase/gridupdater.js
+++ codebase/gridupdater.js
@@ -4,47 +4,76 @@
  */
 
 function updategridfxn() {
-   //deleteRows();deleteColumns();
+   //var addingView = {view:'iframe',src:'viewer.html',minWidth:250,minHeight:250};
+   //addnumCols();
+   //$$('workRows').addView(addingView);
+   //deleteRows();deleteCols();
    //addnumRows();addnumCols();
-   $$('workRows').addView({view:'iframe',src:'viewer.html',minWidth:250,minHeight:250});
-   $$('workCols0').addView({view:'iframe',src:'viewer.html',minWidth:250,minHeight:250});
+   //console.log("original: "+$$('workRows').getChildViews().length);
+   //$$('workLayout').addView({view:'iframe',src:'viewer.html',minWidth:250,minHeight:250});
+   //$$('workCols0').addView({view:'iframe',src:'viewer.html',minWidth:250,minHeight:250});
    
-   console.log($$('workRows'
-                  ).rows().length);
+   //console.log("new: "+$$('workRows').getChildViews().length);
+   
+   /*var newNumRows = $$('grid_dim').getValues().numRow,
+      newNumCols = $$('grid_dim').getValues().numCol;
+   var currentNumRows = $$('workLayout').getChildViews().length,
+      currentNumCols = $$('workRow0').getChildViews().length;
+   
+   if(newNumRows < currentNumRowsRows)
+      deleteRows();
+   else if(newNumRows > currentNumRows)
+      appendRows();
+   if(newNumCols < currentNumCols)
+      deleteCols();
+   else if(newNumCols > currentNumCols)
+      calculateColsCols();
+   else
+      return; //ie if the new dimensions are the same as the current grid size */
+   
+   appendRows();
+   calculateCols();
 }
 
 function deleteRows(){
    
 }
 
-function deleteColumns(){
+function deleteCols(){
    
 }
 
-function addnumRows(){
-	var numRowstoAdd = $$('grid_dim').getValues().numRow-1;
-   //This method overrides everything after the first instance.
-   //TODO: store original size of grid in an array ->
-      //subtract that off instead of 1 -> add only this number to the end of table
+function appendRows(){
+	var numRowstoAdd = $$('grid_dim').getValues().numRow - $$('workLayout').getChildViews().length;
    
-   for(var x=0; x<numRowstoAdd; x++){
-      appendRow();
+   //****THE CURRENT NAMING SYSTEM WON'T WORK AFTER THE FIRST USE OF THIS
+   for(var y=0; y<numRowstoAdd; y++){
+      $$('workLayout').addView({id:"workRow"+(y+1),cols:[{id:"viewer"+"0,"+(y+1),view:'iframe',
+                                    src:'viewer.html', minWidth:250,minHeight:250}]});
+      
+      if($$('grid_dim').getValues().numCol > 1){
+         var numColstoAdd = $$('grid_dim').getValues().numCol - $$('workRow'+y).getChildViews().length;
+         appendCols(y, numColstoAdd);
+      }
    }
 }
 
-function addnumCols(){
-	var numColstoAdd = $$('grid_dim').getValues().numCol-1;
-   //See comments for addnumRows
+function calculateCols(){
+   var numRows = $$('workLayout').getChildViews().length;
+	var numColstoAdd;
    
-	for (var x=0; x<numColstoAdd; x++){
-		appendColumn()
-	}
+   //Go through each row and add columns to the end of each
+   //****THE CURRENT NAMING SYSTEM WON'T WORK AFTER THE FIRST USE OF THIS
+   for(var y=0; y<numRows; y++){
+      numColstoAdd = $$('grid_dim').getValues().numCol - $$('workRow'+y).getChildViews().length;
+      
+      appendCols(y, numColstoAdd);
+   }
 }
 
-function appendRow(){
-   //for(var i=0; i<)
-}
-
-function appendColumn(){
-   for(var x=0; x< $$('workRows').rows.length; x++){}
+function appendCols(y, numColstoAdd) {
+   for (var x=0; x<numColstoAdd; x++){
+         $$('workRow'+y).addView({id:"viewer"+(x+1)+","+(y+1),view:'iframe',src:'viewer.html',
+                                  minWidth:250,minHeight:250});
+   }
 }
