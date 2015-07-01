70a2ab2cf1456e9ed0a4f208a33bf729dab11ccf
csera <csera@ucsd.edu>
2015-06-30 18:30:00 -0700

    Added grid downsizing + misc
    - Changes made in gridupdater.js to allow grid to be shrunk
      -- Added logic checks: only relevant resizing will get called
    - Commented out now extraneous code in viewer.html
    - Attempted to add grid coordinate labels (eg "1,0") to viewers
      -- Could only get this to work with the initial viewer
      -- Added code not needed for labeling the initial viewer is commented
    out
    
    Known Bugs:
    1) Chrome won't accept uploads to iframes when just running Hydra
    locally.  Origins for the primary frame and iframe are both "null"
    -> same origin violation.
    2) Chrome and Firefox won't display more than 16-17 molecular
    models even though the viewers themselves are still responsive.
    
    To Do:
    - Show Webix ID/grid coordinates in each viewer's frame
    - Fix bug #1
    - Allow a file to be uploaded to a selected viewer
    
    #Misc (from before):
    If the mouse is being used for compound manipulation and is moved
    outside the viewer before release, the viewer will continue interacting
    with the mouse whenever cursor is in its bounds until the viewer is
    clicked again.  Consider making a global mouse state variable in the
    future?
    - Note: console now sometimes has the following error
    -- "Error: WebGL: Exceeded 16 live WebGL contexts for this principal,
        losing the least recently used one."
    -- Seems to be from changes made for displaying labels

--- codebase/gridupdater.js
+++ codebase/gridupdater.js
@@ -20,40 +20,70 @@ function updategridfxn() {
    var origNumRows = $$('workLayout').getChildViews().length,
       origNumCols = $$('workRow0').getChildViews().length;
    
-   /*if(newNumRows < currentNumRowsRows)
-      deleteRows();
-   else if(newNumRows > currentNumRows)
-      appendRows();
-   if(newNumCols < currentNumCols)
-      deleteCols();
-   else if(newNumCols > currentNumCols)
-      calculateColsCols();
+   if(newNumRows < origNumRows)
+      deleteRows(newNumRows, origNumRows);
+   else if(newNumRows > origNumRows)
+      appendRows(newNumRows, origNumRows);
+   if(newNumCols < origNumCols)
+      deleteCols(newNumCols, origNumCols);
+   else if(newNumCols > origNumCols)
+      calculateCols();
    else
-      return; //ie if the new dimensions are the same as the current grid size */
+      return; //ie if the new dimensions are the same as the current grid size
    
-   appendRows(newNumRows, origNumRows);
-   calculateCols();
+   /*appendRows(newNumRows, origNumRows);
+   calculateCols();*/
 }
 
-function deleteRows(){
-   
+function deleteRows(newNumRows, origNumRows){
+   //Delete rows using a for loop index = row index
+   for(var y=origNumRows-1; y>=newNumRows; y--){
+      console.log("deleting workRow"+y);
+      $$('workLayout').removeView('workRow'+y);
+   }
 }
 
-function deleteCols(){
+function deleteCols(newNumCols, origNumCols){
+   var numRows = $$('workLayout').getChildViews().length;
    
+   for(var y=numRows-1; y>=0; y--){
+      for(var x=origNumCols-1; x>=newNumCols; x--){
+         $$('workRow'+y).removeView('viewer'+x+','+y);
+      }
+   }
 }
 
 function appendRows(newNumRows, origNumRows){
 	var numRowstoAdd = newNumRows - origNumRows;
    
    for(var y=0; y<numRowstoAdd; y++){
-      $$('workLayout').addView({id:"workRow"+(origNumRows+y),cols:[{id:"viewer"+"0,"+(origNumRows+y),
-                                    view:'iframe', src:'viewer.html', minWidth:250,minHeight:250}]});
+      $$('workLayout').addView({id:"workRow"+(origNumRows+y),cols:[
+         {id:"viewer0,"+(origNumRows+y),view:'iframe', src:'viewer.html',
+            minWidth:250,minHeight:250,
+            /*on:{'onAfterLoad':function(){
+               console.log("         after y="+y);
+               console.log("         id="+this.id);
+					//this.getWindow().setGridCoordinates('0,'+(origNumRows+y));
+               this.getWindow().setGridCoordinates(this.id);
+				}}*/
+         }
+      ]});
       //No need for +1 to id # because element length starts at 1
       
+      //gives $$(...) is undefined
+      /*$$('viewer0,'+(origNumRows+y)).attachEvent("onAfterLoad",function(){
+         console.log("     rows id");
+         $$('viewer0,'+(origNumRows+y)).getWindow().setGridCoordinates('0,'+(origNumRows+y));
+      });*/
+      console.log((origNumRows+y));
+      
+      //gives $$(...).getWindow(...).setGridCoordinates is not a function
+      //$$('viewer0,'+(origNumRows+y)).getWindow().setGridCoordinates('0,'+(origNumRows+y));
+         
       if($$('grid_dim').getValues().numCol > 1){
          var numColstoAdd = $$('grid_dim').getValues().numCol -
             $$('workRow'+(origNumRows+y)).getChildViews().length;
+         
          appendCols((origNumRows+y), numColstoAdd);
       }
    }
@@ -76,5 +106,11 @@ function appendCols(y, numColstoAdd) {
    for (var x=0; x<numColstoAdd; x++){
          $$('workRow'+y).addView({id:"viewer"+(x+1)+","+(y),view:'iframe',src:'viewer.html',
                                   minWidth:250,minHeight:250});
+         
+         /*$$('viewer'+(x+1)+','+(y)).attachEvent("onAfterLoad",function(){
+            console.log("     cols id");
+            $$('viewer'+(x+1)+','+(y)).getWindow().setGridCoordinates((x+1)+','+(y));
+         });*/
+         //$$("viewer"+(x+1)+","+(y)).getWindow().setGridCoordinates((x+1)+','+(y));
    }
 }
