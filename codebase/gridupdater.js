4cf3a8d280099a713f8ced887d450c5b8d69a8bb
csera <csera@ucsd.edu>
2015-07-03 12:07:57 -0700

    Cleanup & Bug Fixes: Displayed Viewer Name
    - Viewer names were being given correctly. However, the initially
       added viewer for new rows (ie leftmost viewer) was being passed
       an incorrect value for display.  Fixed this
    - Misc code cleanup with comments
    
    Known Bugs:
    1) Chrome won't accept uploads to iframes when just running Hydra
    locally.  Origins for the primary frame and iframe are both "null"
      -> same origin violation.
      - This actually works now? Possibly due to a Chrome update.
    2) Chrome and Firefox won't display more than 16-17 molecular
    models even though the viewers themselves are still responsive.
      - "Error: WebGL: Exceeded 16 live WebGL contexts for this
         principal, losing the least recently used one."
      - This is due to limitations built into Three49custom.js
    
    To Do:
    - Improve controls for uploading files to viewers
      -- Replace primitive controls with a file list + controls
      -- Try implementing drag-and-drop
    - Investigate mysterious resolving of bug #1
    - Add padding between cells for easier scrolling on Macs
    - Pull mouse mode setting (eg "Rotate", "Translate", etc)
      out of viewers. Universal control in Hydra GUI
    - Replace "ID" in "Compound List" with coordinates
    
    Misc (from before):
    If the mouse is being used for compound manipulation and is moved
    outside the viewer before release, the viewer will continue interacting
    with the mouse whenever cursor is in its bounds until the viewer is
    clicked again.  Consider making a global mouse state variable in the
    future?

--- codebase/gridupdater.js
+++ codebase/gridupdater.js
@@ -4,17 +4,6 @@
  */
 
 function updategridfxn() {
-   //var addingView = {view:'iframe',src:'viewer.html',minWidth:250,minHeight:250};
-   //addnumCols();
-   //$$('workRows').addView(addingView);
-   //deleteRows();deleteCols();
-   //addnumRows();addnumCols();
-   //console.log("original: "+$$('workRows').getChildViews().length);
-   //$$('workLayout').addView({view:'iframe',src:'viewer.html',minWidth:250,minHeight:250});
-   //$$('workCols0').addView({view:'iframe',src:'viewer.html',minWidth:250,minHeight:250});
-   
-   //console.log("new: "+$$('workRows').getChildViews().length);
-   
    var newNumRows = $$('grid_dim').getValues().numRow,
       newNumCols = $$('grid_dim').getValues().numCol;
    var origNumRows = $$('workLayout').getChildViews().length,
@@ -24,7 +13,7 @@ function updategridfxn() {
    if(newNumRows < origNumRows)
       deleteRows(newNumRows, origNumRows);
    else if(newNumRows > origNumRows){
-      appendRows(newNumRows, origNumRows, newNumCols, origNumCols);
+      appendRows(newNumRows, origNumRows, newNumCols);
       resized = true;
    }
       
@@ -35,59 +24,58 @@ function updategridfxn() {
       resized = true;
    }
    
-   /*else
-      return;*/ //ie if the new dimensions are the same as the current grid size
+   else //If nothing actually changed, exit the function
+      return;
    
 }
 
 function deleteRows(newNumRows, origNumRows){
-   //Delete rows using a for loop index = row index
+   
    for(var y=origNumRows; y>newNumRows; y--){
-      console.log("deleting workRow"+y);
       $$('workLayout').removeView('workRow'+y);
    }
 }
 
 function deleteCols(newNumCols, origNumCols){
    var numRows = $$('workLayout').getChildViews().length;
-   console.log("deleting cols");
+   
+   //Delete in EVERY row
    for(var y=numRows; y>0; y--){
-      console.log("on row "+y);
-      for(var x=origNumCols; x>newNumCols; x--){
-         console.log("          deleting viewer"+x+','+y);
+      for(var x=origNumCols; x>newNumCols; x--){ //Delete at ends on the right
          $$('workRow'+y).removeView('viewer'+x+','+y);
       }
    }
 }
 
-function appendRows(newNumRows, origNumRows, newNumCols, origNumCols){
+function appendRows(newNumRows, origNumRows, newNumCols){
 	var numRowstoAdd = newNumRows - origNumRows;
    
+   //Non-ideal implementation: using a loop var not = the index var
+   //Making it = the index var caused bugs though...
    for(var y=1; y<=numRowstoAdd; y++){
       $$('workLayout').addView({id:"workRow"+(origNumRows+y), cols:[
          {id:"viewer0,"+(origNumRows+y), view:'iframe', src:'viewer.html',
             minWidth:250, minHeight:250,
-            on:{'onAfterLoad':setCoord_callback(1,y)}
+            on:{'onAfterLoad':setCoord_callback(1,(origNumRows+y))}
          }
       ]});
       
-      if($$('grid_dim').getValues().numCol > 1){
+      if(newNumCols > 1){
          appendCols((origNumRows+y),newNumCols,1)
       }
    }
 }
 
 function calculateCols(newNumCols){
-   //using a unique, local var so that it only goes through what is actually there
+   //Using a unique, local var so that it only goes through what is actually there
+   //(number of rows may have changed since initial check)
    var numRows = $$('workLayout').getChildViews().length;
    var numCols;
 	var numColstoAdd;
    
-   //Go through each row and add columns to the end of each
+   //Go through EVERY row and add columns to the end of each
    for(var y=1; y<=numRows; y++){
-      //numColstoAdd = $$('grid_dim').getValues().numCol - $$('workRow'+y).getChildViews().length;
       numCols = $$('workRow'+y).getChildViews().length;
-      //appendCols(y, numColstoAdd);
       appendCols(y,newNumCols,numCols);
    }
 }
