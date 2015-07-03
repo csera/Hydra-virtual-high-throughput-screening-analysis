442f6d9cbcda640dbfe1a57f3a23c69dcfba73df
csera <csera@ucsd.edu>
2015-07-02 17:56:38 -0700

    Primitive Upload Control & ID algorithm fix
    -Added primitive controls for controlling where the most recently
      uploaded file is displayed
      --Created "Upload to Viewer" panel above "Import Compounds"
    -Fixed system for assigning ID's
      --Previously was the source of bugs when the grid was resized
         more than once.  Consequently affected uploading
    -Currently have code for displaying coordinates in iframes
      commented out
    -Swapped the positions of "Columns" and "Rows" in the control panel
      --More intuitive given that coordinates are (x,y) -> (column, row)
    
    Known Bugs:
    1) Chrome won't accept uploads to iframes when just running Hydra
    locally.  Origins for the primary frame and iframe are both "null"
    -> same origin violation.
    -This actually works now? Possibly due to a Chrome update.
    2) Chrome and Firefox won't display more than 16-17 molecular
    models even though the viewers themselves are still responsive.
      - "Error: WebGL: Exceeded 16 live WebGL contexts for this
         principal, losing the least recently used one."
      - This is due to limitations built into Three49custom.js
    
    To Do:
    - Show Webix ID/grid coordinates in each viewer's frame
    - Improve controls for uploading files to viewers
      -- Replace primitive controls with a file list + controls
      -- Try implementing drag-and-drop
    - Investigate mysterious resolving of bug #1
    - Remove "Dock File" box
    - Add padding between cells for easier scrolling on Macs
    - Pull mouse mode setting (eg "Rotate", "Translate", etc)
      out of viewers. Universal control in Hydra GUI
    - Replace "ID" in "Compound List" with coordinates
    
    Misc (from before):
    If the mouse is being used for compound manipulation and is moved
    outside the viewer before release, the viewer will continue interacting
    with the mouse whenever cursor is in its bounds until the viewer is
    clicked again.  Consider making a global mouse state variable in the
    future?

--- codebase/gridupdater.js
+++ codebase/gridupdater.js
@@ -19,20 +19,28 @@ function updategridfxn() {
       newNumCols = $$('grid_dim').getValues().numCol;
    var origNumRows = $$('workLayout').getChildViews().length,
       origNumCols = $$('workRow0').getChildViews().length;
+   var resized = false;
    
    if(newNumRows < origNumRows)
       deleteRows(newNumRows, origNumRows);
-   else if(newNumRows > origNumRows)
-      appendRows(newNumRows, origNumRows);
+   else if(newNumRows > origNumRows){
+      appendRows(newNumRows, origNumRows, newNumCols, origNumCols);
+      resized = true;
+   }
+      
    if(newNumCols < origNumCols)
       deleteCols(newNumCols, origNumCols);
-   else if(newNumCols > origNumCols)
-      calculateCols();
-   else
-      return; //ie if the new dimensions are the same as the current grid size
+   else if(newNumCols > origNumCols){
+      calculateCols(newNumCols);
+      resized = true;
+   }
    
-   /*appendRows(newNumRows, origNumRows);
-   calculateCols();*/
+   /*if (resized)
+      labelViews(newNumRows,newNumCols);*/
+   
+   /*else
+      return;*/ //ie if the new dimensions are the same as the current grid size
+   
 }
 
 function deleteRows(newNumRows, origNumRows){
@@ -45,27 +53,29 @@ function deleteRows(newNumRows, origNumRows){
 
 function deleteCols(newNumCols, origNumCols){
    var numRows = $$('workLayout').getChildViews().length;
-   
+   console.log("deleting cols");
    for(var y=numRows-1; y>=0; y--){
+      console.log("on row "+y);
       for(var x=origNumCols-1; x>=newNumCols; x--){
+         console.log("          deleting viewer"+x+','+y);
          $$('workRow'+y).removeView('viewer'+x+','+y);
       }
    }
 }
 
-function appendRows(newNumRows, origNumRows){
+function appendRows(newNumRows, origNumRows, newNumCols, origNumCols){
 	var numRowstoAdd = newNumRows - origNumRows;
    
    for(var y=0; y<numRowstoAdd; y++){
-      $$('workLayout').addView({id:"workRow"+(origNumRows+y),cols:[
-         {id:"viewer0,"+(origNumRows+y),view:'iframe', src:'viewer.html',
-            minWidth:250,minHeight:250,
-            /*on:{'onAfterLoad':function(){
+      $$('workLayout').addView({id:"workRow"+(origNumRows+y), cols:[
+         {id:"viewer0,"+(origNumRows+y), view:'iframe', src:'viewer.html',
+            minWidth:250, minHeight:250,
+            on:{'onAfterLoad':function(){
                console.log("         after y="+y);
-               console.log("         id="+this.id);
-					//this.getWindow().setGridCoordinates('0,'+(origNumRows+y));
-               this.getWindow().setGridCoordinates(this.id);
-				}}*/
+               console.log("         loaded id="+this.id);
+					this.getWindow().setGridCoordinates('0,'+(origNumRows+y));
+               //this.getWindow().setGridCoordinates(this.id);
+				}}
          }
       ]});
       //No need for +1 to id # because element length starts at 1
@@ -75,42 +85,61 @@ function appendRows(newNumRows, origNumRows){
          console.log("     rows id");
          $$('viewer0,'+(origNumRows+y)).getWindow().setGridCoordinates('0,'+(origNumRows+y));
       });*/
-      console.log((origNumRows+y));
       
       //gives $$(...).getWindow(...).setGridCoordinates is not a function
-      //$$('viewer0,'+(origNumRows+y)).getWindow().setGridCoordinates('0,'+(origNumRows+y));
-         
+      //$$('viewer0,'+(origNumRows+y)).getWindow().loadFile('hi');
+      
+      console.log('     MAKING (0,'+(origNumRows+y)+')');
+      
       if($$('grid_dim').getValues().numCol > 1){
-         var numColstoAdd = $$('grid_dim').getValues().numCol -
-            $$('workRow'+(origNumRows+y)).getChildViews().length;
+         /*var numColstoAdd = $$('grid_dim').getValues().numCol -
+            $$('workRow'+(origNumRows+y)).getChildViews().length;*/
          
-         appendCols((origNumRows+y), numColstoAdd);
+         //appendCols((origNumRows+y), numColstoAdd);
+         appendCols((origNumRows+y),newNumCols,1)
       }
    }
 }
 
-function calculateCols(){
+function calculateCols(newNumCols){
+   //using a unique, local var so that it only goes through what is actually there
    var numRows = $$('workLayout').getChildViews().length;
+   var numCols;
 	var numColstoAdd;
    
    //Go through each row and add columns to the end of each
    for(var y=0; y<numRows; y++){
-      numColstoAdd = $$('grid_dim').getValues().numCol - $$('workRow'+y).getChildViews().length;
-      
-      appendCols(y, numColstoAdd);
+      //numColstoAdd = $$('grid_dim').getValues().numCol - $$('workRow'+y).getChildViews().length;
+      numCols = $$('workRow'+y).getChildViews().length;
+      //appendCols(y, numColstoAdd);
+      appendCols(y,newNumCols,numCols);
    }
 }
 
-function appendCols(y, numColstoAdd) {
+//function appendCols(y, numColstoAdd) {
+function appendCols(y, newNumCols, currentNumCols) {
    //The y var that gets passed in is already adjusted so no adjustments are necessary here
-   for (var x=0; x<numColstoAdd; x++){
-         $$('workRow'+y).addView({id:"viewer"+(x+1)+","+(y),view:'iframe',src:'viewer.html',
-                                  minWidth:250,minHeight:250});
+   //Goes through a single row and adds viewers as new columns
+   //for (var x=0; x<numColstoAdd; x++){
+   for(var x=currentNumCols; x<newNumCols; x++){
+      console.log('     MAKING ('+x+','+y+')');
+         $$('workRow'+y).addView({id:"viewer"+x+","+(y), view:'iframe', src:'viewer.html',
+                                  minWidth:250, minHeight:250});
          
          /*$$('viewer'+(x+1)+','+(y)).attachEvent("onAfterLoad",function(){
             console.log("     cols id");
             $$('viewer'+(x+1)+','+(y)).getWindow().setGridCoordinates((x+1)+','+(y));
          });*/
          //$$("viewer"+(x+1)+","+(y)).getWindow().setGridCoordinates((x+1)+','+(y));
+   }
+}
+
+//Clunky workaround for not being able to do this as each iframe is added. Hopefully temporary
+function labelViews(numRows, numCols) {
+   for(y=0; y<numRows; y++){
+      for(x=0; x<numCols; x++){
+         console.log('('+x+','+y+')');
+         $$('viewer'+x+','+y).getWindow().setGridCoordinates(x+','+y);
+      }
    }
 }
